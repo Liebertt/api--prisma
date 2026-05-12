c52106c46a89ca0b93d91684d7df7d5f98af104d
Lieberte Ferreira de Sousa <105327109+Liebertt@users.noreply.github.com>
2026-05-12 16:19:54 +0000

    carregando o projeto

---
+++ prisma/migrations/20260512161941_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Produto" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "preco" DOUBLE PRECISION NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+
+    CONSTRAINT "Produto_pkey" PRIMARY KEY ("id")
+);
